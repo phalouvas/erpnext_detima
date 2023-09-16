71b410f6fd0bb6fb8c52cbd60320ce58c323a859
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-09-16 05:39:00 +0000

    included tax

--- detima/detima/report/project_invoices/project_invoices.py
+++ detima/detima/report/project_invoices/project_invoices.py
@@ -9,8 +9,8 @@ def execute(filters=None):
     columns = []
     columns.append(
 			dict(
-				label=_("Sales Invoice"),
-				fieldname="Sales Invoice",
+				label=_("Invoice"),
+				fieldname="Invoice",
 				fieldtype="Dynamic Link",
 				options=None,
 				width="100",
@@ -18,31 +18,13 @@ def execute(filters=None):
 		)
     columns.append(
 		dict(
-			label=_("Sales Invoice Date"),
-			fieldname="Sales Invoice Date",
+			label=_("Posting Date"),
+			fieldname="Posting Date",
 			fieldtype="Date",
 			options=None,
 			width="100",
 		)
-	)
-    columns.append(
-			dict(
-				label=_("Purchase Invoice"),
-				fieldname="Purchase Invoice",
-				fieldtype="Dynamic Link",
-				options=None,
-				width="100",
-			)
-		)
-    columns.append(
-		dict(
-			label=_("Purchase Invoice Date"),
-			fieldname="Purchase Invoice Date",
-			fieldtype="Date",
-			options=None,
-			width="100",
-		)
-	)
+	)    
     columns.append(
 			dict(
 				label=_("Invoice Type"),
@@ -63,8 +45,26 @@ def execute(filters=None):
 		)
     columns.append(
 			dict(
-				label=_("Running Balance"),
-				fieldname="Running Balance",
+				label=_("Amount Running Balance"),
+				fieldname="Amount Running Balance",
+				fieldtype="Currency",
+				options="Company:company:default_currency",
+				width="100",
+			)
+		)
+    columns.append(
+			dict(
+				label=_("Tax"),
+				fieldname="Tax",
+				fieldtype="Currency",
+				options="Company:company:default_currency",
+				width="100",
+			)
+		)
+    columns.append(
+			dict(
+				label=_("Tax Running Balance"),
+				fieldname="Tax Running Balance",
 				fieldtype="Currency",
 				options="Company:company:default_currency",
 				width="100",
@@ -80,37 +80,35 @@ def execute(filters=None):
     # Query for sales invoices sorted by posting date
     query_sales = """
     SELECT
-        si.name AS 'Sales Invoice',
-        si.posting_date AS 'Sales Invoice Date',
-        NULL AS 'Purchase Invoice',
-        NULL AS 'Purchase Invoice Date',
+        si.name AS 'Invoice',
+        si.posting_date AS 'Posting Date',
         'Sales' AS 'Invoice Type',
-        si.total AS 'Amount'
+        si.total AS 'Amount',
+        si.total_taxes_and_charges AS 'Tax'
     FROM
         `tabSales Invoice` si
     WHERE
         si.project = %(project_name)s
         AND si.posting_date BETWEEN %(date_from)s AND %(date_to)s
     ORDER BY 
-        'Sales Invoice Date'
+        'Posting Date'
     """
 
     # Query for purchase invoices sorted by posting date
     query_purchase = """
     SELECT
-        NULL AS 'Sales Invoice',
-        NULL AS 'Sales Invoice Date',
-        pi.name AS 'Purchase Invoice',
-        pi.posting_date AS 'Purchase Invoice Date',
+        pi.name AS 'Invoice',
+        pi.posting_date AS 'Posting Date',
         'Purchase' AS 'Invoice Type',
-        pi.total AS 'Amount'
+        pi.total AS 'Amount',
+        pi.total_taxes_and_charges AS 'Tax'
     FROM
         `tabPurchase Invoice` pi
     WHERE
         pi.project = %(project_name)s
         AND pi.posting_date BETWEEN %(date_from)s AND %(date_to)s
     ORDER BY 
-        'Purchase Invoice Date'
+        'Posting Date'
     """
 
     # Execute both SQL queries
@@ -118,23 +116,27 @@ def execute(filters=None):
     query_purchase_result = frappe.db.sql(query_purchase, {"project_name": project_name, "date_from": date_from, "date_to": date_to}, as_dict=True)
 
     # Merge the results and sort them by date
-    merged_result = sorted(query_sales_result + query_purchase_result, key=lambda x: x.get('Sales Invoice Date') or x.get('Purchase Invoice Date'))
+    merged_result = sorted(query_sales_result + query_purchase_result, key=lambda x: x.get('Posting Date'))
 
-    running_balance = 0
+    amount_running_balance = 0
+    tax_running_balance = 0
     total = 0  # Initialize the total
 
     for row in merged_result:
         if row.get('Invoice Type') == 'Sales':
-            running_balance += row.get('Amount')
+            amount_running_balance += row.get('Amount')
+            tax_running_balance += row.get('Tax')
             total += row.get('Amount')  # Add the amount to the total for sales invoices
         elif row.get('Invoice Type') == 'Purchase':
-            running_balance -= row.get('Amount')
+            amount_running_balance -= row.get('Amount')
+            tax_running_balance -= row.get('Tax')
             total -= row.get('Amount')  # Subtract the amount from the total for purchase invoices
 
         # Add the 'Total' column to the row
         row['Total'] = total
 
-        row['Running Balance'] = running_balance
+        row['Amount Running Balance'] = amount_running_balance
+        row['Tax Running Balance'] = tax_running_balance
         data.append(row)
 
     return columns, data
